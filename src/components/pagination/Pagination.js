98923490811dfcec3bc97032ff05049ec28a96f0
Menchu MV <mcmvillalobos@gmail.com>
2021-10-18 18:25:17 +0200

    Added increment and decrement function

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
+
 const Pagination = () => {
+  const [page, setPage] = useState(1);
+
+  const increment = () => {
+    if (page < 6) {
+      setPage(page + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
   return (
     <div className="pageNumbers">
-      <div className="button">less</div>
-      <div className="number">{number}</div>
-      <div className="number">{number}</div>
-      <div className="number">{number}</div>
-      <div className="number">{number}</div>
-      <div className="number">{number}</div>
-      <div className="button">more</div>
+      <div className="button" onClick={decrement}>
+        «
+      </div>
+      <div className="pageNumber">{page}</div>
+      <div className="pageNumber">{page + 1}</div>
+      <div className="pageNumber">{page + 2}</div>
+      <div className="pageNumber">{page + 3}</div>
+      <div className="pageNumber">{page + 4}</div>
+      <div className="button" onClick={increment}>
+        »
+      </div>
     </div>
   );
 };
