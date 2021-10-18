2028f09eb8532b561e512dd9b319999389e0cfd0
Menchu MV <mcmvillalobos@gmail.com>
2021-10-18 18:17:48 +0200

    Added Pagination component

---
+++ src/components/pagination/Pagination.js
@@ -0,0 +1,15 @@
+const Pagination = () => {
+  return (
+    <div className="pageNumbers">
+      <div className="button">less</div>
+      <div className="number">{number}</div>
+      <div className="number">{number}</div>
+      <div className="number">{number}</div>
+      <div className="number">{number}</div>
+      <div className="number">{number}</div>
+      <div className="button">more</div>
+    </div>
+  );
+};
+
+export default Pagination;
